a1b9d5ffb3e17aca4587ccad58101e4e72e434a3
projectsky5 <mackvej@gmail.com>
2025-07-10 16:03:54 +0300

    Initial commit

---
+++ src/main/java/com/projectsky/reactiveorders/ReactiveOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.projectsky.reactiveorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveOrdersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectsky/reactiveorders/ReactiveOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsky.reactiveorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
